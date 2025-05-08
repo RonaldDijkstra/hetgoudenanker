6bad1f24a1c1e5f69b46e3cc970e0a6c358563f6
Ronald Dijkstra <ronald.dijkstra@voys.nl>
2025-05-08 22:44:54 +0200

    feat: Register custom admin plugin

---
+++ web/app/plugins/custom-admin/custom-admin.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name:     Admin Tweaks
+ * Description:     All tweaks for the WordPress Admin
+ * Author:          Ronald Dijkstra
+ * Version:         0.0.1
+ * Text Domain:     custom-admin
+ * Domain Path:     /languages
+ * License:         GPLv2
+ *
+ * @package         Custom_Admin
+ */
